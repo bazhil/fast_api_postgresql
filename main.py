031a7b0ec0fc173a07ea68bc3efe2bb397e7ae3a
bazhil <i-bazh@mail.ru>
2023-02-19 01:05:37 +0300

    TODO: Need create PG table from console + add rights to users, fow work from DBeaver

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    """Serializer"""
+    id: int
+    name: str
+    description: str
+    price: int
+    on_offer: bool
 
 
 @app.get('/')
@@ -12,4 +23,17 @@ def index():
 @app.get('/greet/{name}')
 def greet_name(name: str):
     return {'greeting': f'Hello, {name}!'}
+
+
+@app.get('/greet')
+def greet_optional_name(name: Optional[str]='user'):
+    return {'Message': f'Hello, {name}'}
+
+
+@app.put('/item/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'Name': item.name,
+            'Description': item.description,
+            'Price': item.price,
+            'on_offer': item.on_offer}
 
