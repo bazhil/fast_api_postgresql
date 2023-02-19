0a73509ca1b81ef16842e4228ba2ca661a6aa1d6
bazhil <i-bazh@mail.ru>
2023-02-19 22:44:30 +0300

    Add model and database

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:postgres@localhost/fast_api_postgresql')
+engine = create_engine('postgresql://postgres:postgres@localhost/fast_api_postgresql',
+                       echo=True)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
